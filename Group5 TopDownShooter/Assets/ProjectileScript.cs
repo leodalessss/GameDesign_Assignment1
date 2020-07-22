5e1270e0fe6c8eb3fdd7bb58b1dea4f86f48dfa4
Lorrah <1711772@students.wits.ac.za>
2020-07-22 22:09:31 +0200

    Added Box

---
+++ Group5 TopDownShooter/Assets/ProjectileScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour
+{
+   public  float force=30f;
+    Rigidbody projRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        projRb = GetComponent<Rigidbody>();
+        projRb.AddForce(transform.up * force, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
