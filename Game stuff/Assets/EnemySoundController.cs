829333fb1dbfd276b1414a588fce30ebff18ad95
JessWhosBack <60912625+JessWhosBack@users.noreply.github.com>
2020-07-23 16:21:15 +0200

    A small start to the sound effects

---
+++ Game stuff/Assets/EnemySoundController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySoundController : MonoBehaviour
+{
+    AudioSource myAudioSource;
+    int rand;
+    public AudioClip[] myAudioClip;
+
+    void Start()
+    {
+        myAudioSource = this.GetComponent<AudioSource>();
+        rand = Random.Range(0, myAudioClip.Length);
+        myAudioSource.clip = myAudioClip[rand];
+        myAudioSource.Play();
+    }
+}
